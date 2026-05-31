ff0da7f21918d61b048dfe7cbe32720e79d3dff4
RavirajPawar <pawarraviraj9160@gmail.com>
2026-05-31 17:00:47 +0530

    flag controlled file_logging

--- app/logger/__init__.py
+++ app/logger/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import datetime
+import os
 
 from architecture.singleton import Singleton
 
@@ -8,31 +9,32 @@ from architecture.singleton import Singleton
 class CustomLogger(metaclass=Singleton):
     """logger class which creates logging class object"""
 
-    def __init__(self):
-        self.custom_logger = logging.getLogger(__name__)
-        self.custom_logger.setLevel(logging.DEBUG)
-
-        # Get the current date and time
-        now = datetime.datetime.now()
-        current_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
-
-        # Create a file handler with log file name including current date and time
-        log_file = f"log/app_{current_datetime}.log"
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.DEBUG)
+    def __init__(self, enable_file_logging: bool = False):
         log_formatter = logging.Formatter(
             "[%(asctime)s] - [%(levelname)s] - [%(filename)s:%(lineno)d] - %(message)s"
         )
-        file_handler.setFormatter(log_formatter)
-
+        self.custom_logger = logging.getLogger(__name__)
+        self.custom_logger.setLevel(logging.DEBUG)
         # Create a stream handler for printing logs to the console
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setLevel(logging.DEBUG)
         stream_handler.setFormatter(log_formatter)
+        self.custom_logger.addHandler(stream_handler)
+        self.file_logging() if enable_file_logging else None
 
+    def file_logging(self):
+        # Useful to save logs in log file when log stream is not available
+
+        now = datetime.datetime.now()
+        current_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
+        # Create a file handler with log file name including current date and time
+        log_file = f"log/app_{current_datetime}.log"
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(log_formatter)
         # Add both handlers to the logger
         self.custom_logger.addHandler(file_handler)
-        self.custom_logger.addHandler(stream_handler)
 
 
 logger = CustomLogger().custom_logger
